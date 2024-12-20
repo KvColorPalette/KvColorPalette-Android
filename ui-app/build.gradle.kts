a656238e7729d2ff0d5ee203f0b03778a82f3951
Kavimal Wijewardana <kavimalw@gmail.com>
2024-12-20 11:01:30 -0500

    Color Pallet: Update the group-id of the artifact.

--- ui-app/build.gradle.kts
+++ ui-app/build.gradle.kts
@@ -52,10 +52,9 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.compose.material)
+    implementation(libs.androidx.navigation.compose)
 
     implementation(project(":kv-color-pallet"))
-
-    implementation("androidx.navigation:navigation-compose:2.8.5")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
