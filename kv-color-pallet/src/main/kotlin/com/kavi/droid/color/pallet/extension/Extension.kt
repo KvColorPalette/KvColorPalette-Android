e14c895f869b3a9813d61f1fa1e14a9ddb12fce9
Kavimal Wijewardana <kavimalw@gmail.com>
2025-01-10 22:21:00 -0500

    Color Pallet: Added method comments to the code and make the extension attribute internal to the library.

--- kv-color-pallet/src/main/kotlin/com/kavi/droid/color/pallet/extension/Extension.kt
+++ kv-color-pallet/src/main/kotlin/com/kavi/droid/color/pallet/extension/Extension.kt
@@ -4,15 +4,29 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.core.graphics.ColorUtils
 
-val Color.hsl: HSL
+/**
+ * Extension function to get HSL properties of a [Color]
+ *
+ * This [hsl] attribute to the [androidx.compose.ui.graphics.Color] object will provide hue, saturation and lightness properties of the color.
+ */
+internal val Color.hsl: HSL
     get() = getHslProperties(this)
 
+/**
+ * Data class to hold the hue, saturation and lightness properties of a [Color]
+ */
 data class HSL(
     val hue: Float,
     val saturation: Float,
     val lightness: Float
 )
 
+/**
+ * This method extract and returns the hue, saturation and lightness properties of a [Color]
+ *
+ * @param color The color to extract the hue, saturation and lightness properties from.
+ * @return A [HSL] object containing the hue, saturation and lightness properties of the given [Color].
+ */
 private fun getHslProperties(color: Color): HSL {
     val outHsl = floatArrayOf(0f, 0f, 0f)
 
