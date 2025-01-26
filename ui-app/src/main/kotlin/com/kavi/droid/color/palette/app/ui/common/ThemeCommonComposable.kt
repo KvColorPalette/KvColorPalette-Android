e26c70afc193f6e52a24e7d8d764baa64b5e58c3
Kavimal Wijewardana <kavimalw@gmail.com>
2025-01-26 17:24:09 -0500

    Create UI for theme detail screen.

--- ui-app/src/main/kotlin/com/kavi/droid/color/palette/app/ui/common/ThemeCommonComposable.kt
+++ ui-app/src/main/kotlin/com/kavi/droid/color/palette/app/ui/common/ThemeCommonComposable.kt
@@ -7,12 +7,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,8 +23,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.kavi.droid.color.palette.KvColorPalette
+import com.kavi.droid.color.palette.extension.isHighLightColor
+import com.kavi.droid.color.palette.util.ColorUtil
+import java.util.Locale
 
 @Composable
 fun ColorCircle(givenColor: Color, colorLetter: String = "", letterColor: Color = MaterialTheme.colorScheme.onPrimary) {
@@ -116,6 +123,42 @@ fun ThemeColorRow(givenColor: Color) {
                     ColorCircle(appThemeColorSet.dark.quaternary, colorLetter = "Q", letterColor = Color.Black)
                     ColorCircle(appThemeColorSet.dark.background, colorLetter = "B")
                 }
+            }
+        }
+    }
+}
+
+@Composable
+fun ThemeColorItem(color: Color, itemName: String) {
+    Box (
+        modifier = Modifier
+            .height(60.dp)
+            .fillMaxWidth()
+            .background(color = color)
+    ) {
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(start = 8.dp, end = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = itemName,
+                textAlign = TextAlign.Start,
+                fontSize = 12.sp,
+                color = if (color.isHighLightColor) Color.Black else Color.White
+            )
+
+            SelectionContainer {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    text = ColorUtil.getHexWithAlpha(color),
+                    textAlign = TextAlign.End,
+                    fontSize = 14.sp,
+                    color = if (color.isHighLightColor) Color.Black else Color.White
+                )
             }
         }
     }
--- ui-app/src/main/kotlin/com/kavi/droid/color/palette/app/ui/detail/PaletteGenDetailUI.kt
+++ ui-app/src/main/kotlin/com/kavi/droid/color/palette/app/ui/detail/PaletteGenDetailUI.kt
@@ -1,25 +1,17 @@
 package com.kavi.droid.color.palette.app.ui.detail
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
@@ -31,29 +23,25 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.kavi.droid.color.palette.KvColorPalette
-import com.kavi.droid.color.palette.app.R
 import com.kavi.droid.color.palette.app.model.PaletteType
 import com.kavi.droid.color.palette.app.theme.KvColorPaletteTheme
 import com.kavi.droid.color.palette.app.ui.common.ColorStrip
+import com.kavi.droid.color.palette.app.ui.common.SelectedColorUI
 import com.kavi.droid.color.palette.app.ui.common.picker.KvColorPickerBottomSheet
-import com.kavi.droid.color.palette.extension.isHighLightColor
 import com.kavi.droid.color.palette.util.ColorUtil
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PaletteGenDetailUI(paletteType: PaletteType) {
 
-    var selectedColor by remember { mutableStateOf(Color.White) }
-    var colorHex by remember { mutableStateOf(TextFieldValue("")) }
+    val selectedColor = remember { mutableStateOf(Color.White) }
+    val colorHex = remember { mutableStateOf(TextFieldValue("")) }
     var colorPalette by remember { mutableStateOf<List<Color>>(emptyList()) }
 
     val showSheet = remember { mutableStateOf(false) }
@@ -87,81 +75,14 @@ fun PaletteGenDetailUI(paletteType: PaletteType) {
                 )
             }
 
-            Row (
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp)
-                    .border(1.dp, Color.White, shape = RoundedCornerShape(8.dp))
-                    .shadow(
-                        elevation = 10.dp,
-                        shape = RoundedCornerShape(8.dp)
-                    )
-                    .background(Color.White)
-                    .padding(12.dp)
-            ) {
-                Row {
-                    Column (
-                        modifier = Modifier
-                            .weight(.75f)
-                    ) {
-                        Text(
-                            modifier = Modifier
-                                .padding(top = 4.dp, bottom = 4.dp),
-                            text = "Select your color",
-                            style = MaterialTheme.typography.titleMedium,
-                            fontSize = 20.sp,
-                        )
+            SelectedColorUI(colorHex, selectedColor, showSheet)
 
-                        Text(
-                            text = "Touch on the color box or type your color-hex " +
-                                    "on below to pick your primary color to generate color palette.",
-                            textAlign = TextAlign.Start,
-                            color = Color.Gray,
-                            style = MaterialTheme.typography.bodySmall,
-                            fontSize = 10.sp
-                        )
-
-                        OutlinedTextField(
-                            modifier = Modifier
-                                .width(200.dp)
-                                .padding(1.dp),
-                            value = colorHex,
-                            maxLines = 1,
-                            label = { Text(text = "Color Hex") },
-                            onValueChange = { newColorHex ->
-                                colorHex = newColorHex
-                                if (ColorUtil.validateColorHex(newColorHex.text))
-                                    selectedColor = ColorUtil.getColorFromHex(newColorHex.text)
-                                else {
-                                    TextFieldValue("")
-                                    println("Not Valid")
-                                }
-                            }
-                        )
-                    }
-
-                    // Display the current color in a Box with a MaterialTheme shape
-                    Box(
-                        modifier = Modifier
-                            .padding(12.dp)
-                            .height(80.dp)
-                            .width(80.dp)
-                            .weight(.25f)
-                            .background(selectedColor, shape = MaterialTheme.shapes.large)
-                            .border(2.dp, MaterialTheme.colorScheme.primary, RoundedCornerShape(12.dp))
-                            .clickable {
-                                showSheet.value = true
-                            }
-                    ) {
-                        Icon(
-                            painter = painterResource(R.drawable.icon_click_me),
-                            contentDescription = "Select Color",
-                            tint = if (selectedColor.isHighLightColor) Color.Black else Color.White,
-                            modifier = Modifier
-                                .align(Alignment.Center)
-                        )
-                    }
-                }
+            if (showSheet.value) {
+                KvColorPickerBottomSheet(showSheet = showSheet,
+                    sheetState = sheetState, onColorSelected = {
+                        selectedColor.value = it
+                        colorHex.value = TextFieldValue(ColorUtil.getHex(color = selectedColor.value))
+                    })
             }
 
             Button(
@@ -172,24 +93,16 @@ fun PaletteGenDetailUI(paletteType: PaletteType) {
                 onClick = {
                     colorPalette = when (paletteType) {
                         PaletteType.KV_PALETTE -> {
-                            val closestKvColor = KvColorPalette.instance.findClosestKvColor(givenColor = selectedColor)
+                            val closestKvColor = KvColorPalette.instance.findClosestKvColor(givenColor = selectedColor.value)
                             KvColorPalette.instance.generateColorPalette(givenColor = closestKvColor)
                         }
-                        PaletteType.ALPHA_PALETTE -> KvColorPalette.instance.generateAlphaColorPalette(givenColor = selectedColor)
-                        PaletteType.LIGHTNESS_PALETTE -> KvColorPalette.instance.generateLightnessColorPalette(givenColor = selectedColor)
-                        PaletteType.SATURATION_PALETTE -> KvColorPalette.instance.generateSaturationColorPalette(givenColor = selectedColor)
+                        PaletteType.ALPHA_PALETTE -> KvColorPalette.instance.generateAlphaColorPalette(givenColor = selectedColor.value)
+                        PaletteType.LIGHTNESS_PALETTE -> KvColorPalette.instance.generateLightnessColorPalette(givenColor = selectedColor.value)
+                        PaletteType.SATURATION_PALETTE -> KvColorPalette.instance.generateSaturationColorPalette(givenColor = selectedColor.value)
                     }
                 }
             ) {
                 Text("Generate Palette")
-            }
-
-            if (showSheet.value) {
-                KvColorPickerBottomSheet(showSheet = showSheet,
-                    sheetState = sheetState, onColorSelected = {
-                        selectedColor = it
-                        colorHex = TextFieldValue(ColorUtil.getHex(color = selectedColor))
-                })
             }
 
             Column(
