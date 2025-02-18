f7cb9c3aa87b0576fe102edbe8af1884c48e5753
Kavimal Wijewardana <kavimalw@gmail.com>
2025-02-17 19:58:01 -0500

    Introduce new field extensions to ColorScheme

--- kv-color-palette/src/main/kotlin/com/kavi/droid/color/palette/extension/OpenExtension.kt
+++ kv-color-palette/src/main/kotlin/com/kavi/droid/color/palette/extension/OpenExtension.kt
@@ -1,5 +1,6 @@
 package com.kavi.droid.color.palette.extension
 
+import androidx.compose.material3.ColorScheme
 import androidx.compose.ui.graphics.Color
 
 /**
@@ -7,3 +8,19 @@ import androidx.compose.ui.graphics.Color
  */
 val Color.isHighLightColor: Boolean
     get() = this.hsl.lightness > 0.6f
+
+/**
+ * Providing new extension fields to [ColorScheme]
+ * @see base: Base color is the consumer provided color to generate the theme
+ * @see quaternary: Quaternary color is for the use of using primary color in light mode with contrast color in dark mode
+ * @see shadow: Shadow color is for the use of shadow in light mode and dark mode.
+ */
+var ColorScheme.base: Color
+    get() = Color.White
+    set(value) {}
+var ColorScheme.quaternary: Color
+    get() = Color.White
+    set(value) {}
+var ColorScheme.shadow: Color
+    get() = Color.White
+    set(value) {}
